45834bf2276ed2c4650584fcc1b34b9d4df3d1cb
w-ryan-jung <w.ryan.jung@gmail.com>
2022-09-09 17:04:17 +0200

    update

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -108,8 +108,9 @@ func getPages(baseUrl string) int {
 func getHttpRes(url string) *http.Response {
 	req, err := http.NewRequest("GET", url, nil)
 	checkErr(err)
-	req.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
-	req.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11`)
+	req.Header.Set("user-agent", "golang application")
+	// req.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
+	// req.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11`)
 
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{
